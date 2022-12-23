cdddb67e185eb1549891309af36fbf5eb9f5902a
Joe Freeman <joe@joef.uk>
2022-12-23 13:35:42 +0000

    Define protocol

---
+++ server_ex/lib/topical/protocol.ex
@@ -0,0 +1,47 @@
+defmodule Topical.Protocol do
+  defmodule Request do
+    def decode(text) do
+      case Jason.decode(text) do
+        {:ok, [0, topic, action, args]} ->
+          {:ok, :notify, topic, action, args}
+
+        {:ok, [1, channel_id, topic, action, args]} ->
+          {:ok, :execute, channel_id, topic, action, args}
+
+        {:ok, [2, channel_id, topic]} ->
+          {:ok, :subscribe, channel_id, topic}
+
+        {:ok, [3, channel_id]} ->
+          {:ok, :unsubscribe, channel_id}
+
+        {:ok, _other} ->
+          {:error, :unrecognised_command}
+
+        _other ->
+          {:error, :decode_failure}
+      end
+    end
+  end
+
+  defmodule Response do
+    def encode_error(channel_id, error) do
+      Jason.encode!([0, channel_id, error])
+    end
+
+    def encode_result(channel_id, result) do
+      Jason.encode!([1, channel_id, result])
+    end
+
+    def encode_topic_reset(channel_id, value) do
+      Jason.encode!([2, channel_id, value])
+    end
+
+    def encode_topic_updates(channel_id, updates) do
+      Jason.encode!([
+        3,
+        channel_id,
+        Enum.map(updates, fn {path, value} -> [path, value] end)
+      ])
+    end
+  end
+end
--- server_ex/mix.exs
+++ server_ex/mix.exs
@@ -18,6 +18,8 @@ defmodule Topical.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:jason, "~> 1.4"}
+    ]
   end
 end
