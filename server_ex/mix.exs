452300035cc2a7bf22208323c0c7bd493c1580b4
Joe Freeman <joe@joef.uk>
2022-12-27 15:48:04 +0000

    Update package metadata

--- server_ex/mix.exs
+++ server_ex/mix.exs
@@ -7,7 +7,9 @@ defmodule Topical.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -20,6 +22,20 @@ defmodule Topical.MixProject do
   defp deps do
     [
       {:jason, "~> 1.4"}
+    ]
+  end
+
+  defp description do
+    """
+    Simple server-maintained state synchronisation.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Joe Freeman"],
+      licenses: ["Apache-2.0"],
+      links: %{GitHub: "https://github.com/joefreeman/topical"}
     ]
   end
 end
