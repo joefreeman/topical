791395bc8c694fc3ab5420feea9536d4d1bee7de
Joe Freeman <joe@joef.uk>
2023-02-18 21:30:18 +0000

    Support passing undefined topic

--- client_react/src/useTopic.ts
+++ client_react/src/useTopic.ts
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback, useContext } from "react";
 
 import { Context } from "./provider";
 
-export default function useTopic<T>(topic: string): [
+export default function useTopic<T>(topic: string | undefined): [
   T | undefined,
   {
     notify: (action: string, ...args: any[]) => void;
@@ -13,15 +13,27 @@ export default function useTopic<T>(topic: string): [
   const [value, setValue] = useState<T>();
   const [error, setError] = useState<any>();
   const notify = useCallback(
-    (action: string, ...args: any[]) => socket!.notify(topic, action, ...args),
+    (action: string, ...args: any[]) => {
+      if (typeof topic == "undefined") {
+        throw new Error("topic is undefined");
+      }
+      return socket!.notify(topic, action, ...args);
+    },
     [socket, topic]
   );
   const execute = useCallback(
-    (action: string, ...args: any[]) => socket!.execute(topic, action, ...args),
+    (action: string, ...args: any[]) => {
+      if (typeof topic == "undefined") {
+        throw new Error("topic is undefined");
+      }
+      return socket!.execute(topic, action, ...args);
+    },
     [socket, topic]
   );
   useEffect(() => {
-    return socket?.subscribe(topic, setValue, setError);
+    if (topic) {
+      return socket?.subscribe(topic, setValue, setError);
+    }
   }, [socket, topic]);
   if (error) {
     throw new Error(error);
