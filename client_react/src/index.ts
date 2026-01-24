23d7f5a9ca1a474f295b721bc4789dd62a9326b1
Joe Freeman <joe@joef.uk>
2026-01-24 19:41:35 +0000

    Fix linting issue

--- client_react/src/index.ts
+++ client_react/src/index.ts
@@ -1,4 +1,4 @@
+export type { WebSocketFactory } from "@topical/core";
 export { default as SocketProvider } from "./provider";
 export { default as useSocket } from "./useSocket";
 export { default as useTopic } from "./useTopic";
-export type { WebSocketFactory } from "@topical/core";
