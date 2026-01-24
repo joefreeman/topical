304c87d3c025717c8c16fc779a431264e26d98c4
Joe Freeman <joe@joef.uk>
2026-01-24 19:19:19 +0000

    Support providing a custom WebSocket creation function

--- client_react/src/index.ts
+++ client_react/src/index.ts
@@ -1,3 +1,4 @@
 export { default as SocketProvider } from "./provider";
 export { default as useSocket } from "./useSocket";
 export { default as useTopic } from "./useTopic";
+export type { WebSocketFactory } from "@topical/core";
